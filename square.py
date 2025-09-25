59b473859901b16a61cd9ededeacf41a79435308
JonasMaesBaFys <Jonas.Maes3@student.uantwerpen.be>
2025-09-25 11:44:43 +0200

    cleanup

--- square.py
+++ square.py
@@ -1,18 +1,14 @@
-def square(x):
-    return x**2
-
-a = 3
-
 def solve_pythagorean(upper_z):
     solutions = []
     for z in range(1, upper_z + 1):
         for x in range(1, z):
             for y in range(x, z):
-                if square(x) + square(y) == square(z):
+                if x**2 + y**2 == z**2:
                     solutions.append((x, y, z))
     return solutions
 
 def main():
     print(solve_pythagorean(20))
+
 if __name__ == "__main__":
     main()
